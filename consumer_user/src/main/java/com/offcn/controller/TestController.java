c590c0fbcc8ac2217b6c8d4227a6467c6e605c1a
18235272595 <73531394+18235272595@users.noreply.github.com>
2020-10-27 21:45:10 +0800

    Update TestController.java
    提交测试

--- consumer_user/src/main/java/com/offcn/controller/TestController.java
+++ consumer_user/src/main/java/com/offcn/controller/TestController.java
@@ -3,5 +3,6 @@ package com.offcn.controller;
 public class TestController {
     public static void main(String[] args) {
         System.out.println("======");
+        System.out.println("======");
     }
 }
